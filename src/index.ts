899ef658018f45bdd57cafdfc0abefbe80d97033
Sancho Júnior <sanchojunior.ce@gmail.com>
2026-03-01 19:08:35 -0300

    Add Dotenv and Fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-const name = "";
+// Import the framework and instantiate it
+import "dotenv/config";
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: Number(process.env.PORT) ?? 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
